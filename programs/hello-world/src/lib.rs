1c726fbe42f3df4974aaa276d77fd389facc9e3b
Jorge Hernandez <jorge.hernandez@go1.com>
2022-07-22 00:48:04 -0500

    pda account + counter

--- programs/hello-world/src/lib.rs
+++ programs/hello-world/src/lib.rs
@@ -7,9 +7,54 @@ pub mod hello_world {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.counter_account.init = true;
+        ctx.accounts.counter_account.auth = *ctx.accounts.user.key;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<CounterAction>) -> Result<()> {
+        ctx.accounts.counter_account.counter += 1;
+
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<CounterAction>) -> Result<()> {
+        ctx.accounts.counter_account.counter -= 1;
+
         Ok(())
     }
 }
 
+#[account]
+#[derive(Default)]
+pub struct CounterAccount {
+    pub init: bool,
+    pub counter: u64,
+    pub auth: Pubkey,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init, 
+        payer = user, 
+        space = 8 + 8 + 1 + 32,
+        seeds = [user.key().as_ref(), b"counter".as_ref()], 
+        bump
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CounterAction<'info> {
+    #[account(
+        mut,
+        constraint = counter_account.auth == user.key()
+    )]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
--- tests/hello-world.ts
+++ tests/hello-world.ts
@@ -1,16 +1,98 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { HelloWorld } from "../target/types/hello_world";
+import assert from 'assert';
 
 describe("hello-world", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const keyPair = anchor.web3.Keypair.generate(); 
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+    const [counterAccount, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [keyPair.publicKey.toBuffer(), Buffer.from("counter")],
+      program.programId
+    );
+
+    await provider.connection.confirmTransaction(
+      await (provider.connection.requestAirdrop(keyPair.publicKey, 1_000_000_000))
+    )
+
+    await program.rpc.initialize({
+      accounts: {
+        counterAccount: counterAccount,
+        user: keyPair.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [keyPair]
+    });
+    const account = await program.account.counterAccount.fetch(counterAccount);
+
+    assert.ok(account.init);
+
+  });
+
+  it("increment", async () => {
+
+    const [counterAccount, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [keyPair.publicKey.toBuffer(), Buffer.from("counter")],
+      program.programId
+    );
+
+    await provider.connection.confirmTransaction(
+      await (provider.connection.requestAirdrop(keyPair.publicKey, 1_000_000_000))
+    )
+
+    await program.rpc.increment({
+      accounts: {
+        counterAccount: counterAccount,
+        user: keyPair.publicKey
+      },
+
+      signers: [keyPair]
+    });
+
+    await program.rpc.increment({
+      accounts: {
+        counterAccount: counterAccount,
+        user: keyPair.publicKey
+      },
+
+      signers: [keyPair]
+    });
+
+    const account = await program.account.counterAccount.fetch(counterAccount);
+    assert.equal(account.counter, 2);
+
+  });
+
+  it("decrement", async () => {
+
+    const [counterAccount, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [keyPair.publicKey.toBuffer(), Buffer.from("counter")],
+      program.programId
+    );
+
+    await provider.connection.confirmTransaction(
+      await (provider.connection.requestAirdrop(keyPair.publicKey, 1_000_000_000))
+    )
+
+    await program.rpc.decrement({
+      accounts: {
+        counterAccount: counterAccount,
+        user: keyPair.publicKey
+      },
+
+      signers: [keyPair]
+    })
+
+    const account = await program.account.counterAccount.fetch(counterAccount);
+
+    // counter should be 1 because in the previous test, the counter was incremented by 2
+    assert.equal(account.counter, 1);
   });
 });
